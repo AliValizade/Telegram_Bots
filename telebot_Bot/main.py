0bd89cd5a2b9ad60f48f4a04a4398ca08a5d6964
AliValizade <alivalizade@gmail.com>
2024-04-27 00:36:39 +0330

    Next step handler

--- telebot_Bot/main.py
+++ telebot_Bot/main.py
@@ -6,11 +6,16 @@ load_dotenv()
 TOKEN = os.environ.get("TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
+# Inline Button
 first_button = telebot.types.InlineKeyboardButton('Button 1', url='https://t.me/kia_todo_list_bot')
 second_button = telebot.types.InlineKeyboardButton('Button 2', callback_data='Hi')
 third_button = telebot.types.InlineKeyboardButton('Button 3', callback_data='Bye')
 markup = telebot.types.InlineKeyboardMarkup()
 markup.add(first_button, second_button, third_button)
+
+# Keyboaed
+key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+key_markup.add('Register', 'Two', 'Three')
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -28,24 +33,32 @@ def send_welcome(message):
     bot.send_message(message.chat.id, 'Hi', reply_markup=markup)
 
 
-key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-key_markup.add('One', 'Two', 'Three')
-
-
 @bot.message_handler(commands=['help'])
 def help_me(message):
     bot.reply_to(message, 'What can I do?', reply_markup=key_markup)
 
 
-@bot.message_handler()
-def keyboard(message):
-    if message.text == 'One':
-        bot.send_message(message.chat.id, 'You tapped on One button.')
-    elif message.text == 'Two':
-        bot.send_message(message.chat.id, 'You tapped on Two button.')
-    elif message.text == 'Three':
-        bot.send_message(message.chat.id, 'You tapped on Three button.')
+@bot.message_handler(func=lambda m: True)
+def info(message):
+    if message.text == 'Register':
+        msg = bot.send_message(message.chat.id, 'Please enter your Name:')
+        bot.register_next_step_handler(msg, name)
 
+def name(message):
+    global nm
+    nm = message.text
+    msg = bot.send_message(message.chat.id, 'Enter your Age:')
+    bot.register_next_step_handler(msg, age)
+
+def age(message):
+    global ag
+    ag = message.text
+    msg = bot.send_message(message.chat.id, 'How tall are you?')
+    bot.register_next_step_handler(msg, tall)
+
+def tall(message):
+    tal = message.text
+    msg = bot.send_message(message.chat.id, f'Your name: {nm}\nAge: {ag}\nTall: {tal}')
 
 
 bot.infinity_polling()
