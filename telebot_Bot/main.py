91d1d937df0f76b114bf8d79c90d7b4aaea0ef90
AliValizade <alivalizade@gmail.com>
2024-04-26 22:57:45 +0330

    add inline keyboard

--- telebot_Bot/main.py
+++ telebot_Bot/main.py
@@ -6,9 +6,14 @@ load_dotenv()
 TOKEN = os.environ.get("TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
+first_button = telebot.types.InlineKeyboardButton('Button 1', url='https://t.me/kia_todo_list_bot')
+second_button = telebot.types.InlineKeyboardButton('Button 2', url='https://t.me/kia_todo_list_bot')
+markup = telebot.types.InlineKeyboardMarkup()
+markup.add(first_button, second_button)
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'Hi')
+    bot.send_message(message.chat.id, 'Hi', reply_markup=markup)
 
 
 
