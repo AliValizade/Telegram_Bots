2aac91c58fa099c4d2b7454cddd3aa19c025cce3
AliValizade <alivalizade@gmail.com>
2024-05-04 02:19:47 +0330

    Banning, admin and silencing users

--- GroupAdmin_Bot/main.py
+++ GroupAdmin_Bot/main.py
@@ -23,6 +23,68 @@ def pin(m):
     bot.pin_chat_message(m.chat.id, m.reply_to_message.id)
     bot.reply_to(m, 'پیام موردنظر پین شد')
 
+@bot.message_handler(func=lambda m: m.text == 'افزودن ادمین')
+def promote(m):
+    bot.promote_chat_member(
+        m.chat.id,
+        m.reply_to_message.json['from']['id'],
+        can_change_info= True,
+        can_post_messages= True,
+        can_edit_messages= True, 
+        can_delete_messages= True, 
+        can_invite_users= True, 
+        can_restrict_members= True, 
+        can_pin_messages= True, 
+        can_promote_members= True, 
+        can_manage_chat= True, 
+        can_manage_video_chats= True, 
+        can_manage_voice_chats= True, 
+        can_manage_topics= False,
+    )
+
+@bot.message_handler(func=lambda m: m.text == 'بن')
+def ban(m):
+    bot.ban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
+    bot.reply_to(m, f'کاربر {m.reply_to_message.from_user.id} بن شد')
+
+@bot.message_handler(func=lambda m: m.text == 'حذف بن')
+def ban(m):
+    bot.unban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
+    bot.reply_to(m, f'کاربر {m.reply_to_message.from_user.id} حذف بن شد')
+
+@bot.message_handler(func=lambda m: m.text == 'کیک بن')
+def ban(m):
+    bot.kick_chat_member(m.chat.id, m.reply_to_message.from_user.id, until_date=5, revoke_messages=True)
+    bot.reply_to(m, f'کاربر {m.reply_to_message.from_user.id} بن موقت شد')
+
+@bot.message_handler(func=lambda m: m.text == 'سکوت')
+def restrict(m):
+    bot.restrict_chat_member(m.chat.id, m.reply_to_message.from_user.id, until_date=10,
+                             can_send_messages= False, 
+                             can_send_media_messages= False, 
+                             can_send_polls= False, 
+                             can_send_other_messages= False, 
+                             can_add_web_page_previews= False, 
+                             can_change_info= False, 
+                             can_invite_users= False, 
+                             can_pin_messages= False, 
+                            )
+    bot.reply_to(m, f'کاربر {m.reply_to_message.from_user.id} سکوت شد')
+
+
+@bot.message_handler(func=lambda m: m.text == 'حذف سکوت')
+def derestrict(m):
+    bot.restrict_chat_member(m.chat.id, m.reply_to_message.from_user.id, until_date=10,
+                             can_send_messages= True, 
+                             can_send_media_messages= True, 
+                             can_send_polls= True, 
+                             can_send_other_messages= True, 
+                             can_add_web_page_previews= True, 
+                             can_change_info= True, 
+                             can_invite_users= True, 
+                             can_pin_messages= True, 
+                            )
+    bot.reply_to(m, f'کاربر {m.reply_to_message.from_user.id} حذف سکوت شد')
 
 
 bot.infinity_polling()
