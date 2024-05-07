012946650ee77f1ae50491942603762bc8750036
AliValizade <alivalizade@gmail.com>
2024-05-07 22:22:25 +0330

    Inline Query

--- ChannelAdmin_Bot/main.py
+++ ChannelAdmin_Bot/main.py
@@ -2,6 +2,7 @@ import os
 import datetime
 from dotenv import load_dotenv
 import telebot
+from telebot import types
 
 load_dotenv()
 TOKEN = os.environ.get("TOKEN")
@@ -18,6 +19,31 @@ def forwardingg(m):
     # copy
     bot.copy_message(chat_id=test2, from_chat_id=test1, message_id=m.message_id)
 
+
+# Inline Query
+items = [
+    {'id':'1', 'title':'Todolist_bot', 'description':'ربات برنامه ریزی روزانه\nاگه میخوای کارای روزانه ات رو برنامه ریزی کنی بیا اینجا', 'thumb':'https://imgurl.ir/download.php?file=b771194_650.jpg', 'message':'ربات تلگرام TodoList'},
+    {'id':'2', 'title':'Agahi7/24', 'description':'ربات برنامه ریزی روزانه\nاگه میخوای کارای روزانه ات رو برنامه ریزی کنی بیا اینجا', 'thumb':'https://imgurl.ir/download.php?file=p74120_ali.jpg', 'message':'ربات تلگرام TodoList'},
+    {'id':'3', 'title':'Remote_Ads', 'description':'ربات برنامه ریزی روزانه\nاگه میخوای کارای روزانه ات رو برنامه ریزی کنی بیا اینجا', 'thumb':'https://imgurl.ir/download.php?file=g88280_Capture1.jpg', 'message':'ربات تلگرام TodoList'},
+    {'id':'4', 'title':'Test_bot', 'description':'ربات برنامه ریزی روزانه\nاگه میخوای کارای روزانه ات رو برنامه ریزی کنی بیا اینجا', 'thumb':'https://imgurl.ir/download.php?file=i29182_img17.jpg', 'message':'ربات تلگرام TodoList'},
+]
+
+@bot.inline_handler(lambda query: len(query.query) == 0)
+def handle_inline_query(query):
+    results = []
+    for item in items:
+        result = types.InlineQueryResultArticle(
+            id=item['id'],
+            title=item['title'],
+            description=item['description'],
+            input_message_content=types.InputTextMessageContent(
+                message_text=item['message']
+            ),
+            thumbnail_url=item['thumb']
+        )
+        results.append(result)
+    
+    bot.answer_inline_query(query.id, results)
 
 
 
