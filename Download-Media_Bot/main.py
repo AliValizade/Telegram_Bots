a5596662e02535aa134a5665895a8dbdbe453d88
AliValizade <alivalizade@gmail.com>
2024-05-08 21:12:48 +0330

    Download other media from telegram chennels

--- Download-Media_Bot/main.py
+++ Download-Media_Bot/main.py
@@ -9,10 +9,11 @@ TOKEN = os.environ.get("TOKEN")
 bot = TeleBot(token=TOKEN)
 
 
-# Download photo from telegram channels
+
 def file_name(m: str):
     return m.split('/')[-1]
 
+# Download photo from telegram channels
 @bot.message_handler(content_types=['photo'])
 def download_photo(message: Message):
     file_id = message.photo[-1].file_id
@@ -22,7 +23,53 @@ def download_photo(message: Message):
     with open(f"{file_name(file_path.file_path)}", 'wb') as newfile:
         newfile.write(d_path)
 
+# Download other media from telegram channels
+@bot.message_handler(content_types=['animation', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice'])
+def download_media(message: Message):
+    if message.content_type == 'audio':
+        file_id = message.audio.file_id
+        file_path = bot.get_file(file_id)
+        d_path = bot.download_file(file_path.file_path)
+        with open(f"{file_name(file_path.file_path)}", 'wb') as newfile:
+            newfile.write(d_path)
+    elif message.content_type == 'video':
+        file_id = message.video.file_id
+        file_path = bot.get_file(file_id)
+        d_path = bot.download_file(file_path.file_path)
+        with open(f"{file_name(file_path.file_path)}", 'wb') as newfile:
+            newfile.write(d_path)
+    elif message.content_type == 'voice':
+        file_id = message.voice.file_id
+        file_path = bot.get_file(file_id)
+        d_path = bot.download_file(file_path.file_path)
+        with open(f"{file_name(file_path.file_path)}", 'wb') as newfile:
+            newfile.write(d_path)
+    elif message.content_type == 'video_note':
+        file_id = message.video_note.file_id
+        file_path = bot.get_file(file_id)
+        d_path = bot.download_file(file_path.file_path)
+        with open(f"{file_name(file_path.file_path)}", 'wb') as newfile:
+            newfile.write(d_path)
+    elif message.content_type == 'document':
+        file_id = message.document.file_id
+        file_path = bot.get_file(file_id)
+        d_path = bot.download_file(file_path.file_path)
+        with open(f"{file_name(file_path.file_path)}", 'wb') as newfile:
+            newfile.write(d_path)
+    elif message.content_type == 'animation':
+        file_id = message.animation.file_id
+        file_path = bot.get_file(file_id)
+        d_path = bot.download_file(file_path.file_path)
+        with open(f"{file_name(file_path.file_path)}", 'wb') as newfile:
+            newfile.write(d_path)
+    elif message.content_type == 'sticker':
+        file_id = message.sticker.file_id
+        file_path = bot.get_file(file_id)
+        d_path = bot.download_file(file_path.file_path)
+        with open(f"{file_name(file_path.file_path)}", 'wb') as newfile:
+            newfile.write(d_path)
 
+    
 
 
 
