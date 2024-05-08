b50b60cbeee0dc2929d0dfaf07e960c7c6e1ee8c
AliValizade <alivalizade@gmail.com>
2024-05-08 18:08:27 +0330

    Reply to message in another chat

--- re-action_in_telebot/main.py
+++ re-action_in_telebot/main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from telebot import TeleBot
-from telebot.types import ReactionTypeEmoji
+from telebot.types import ReactionTypeEmoji, ReplyParameters
 from telebot.util import update_types
 
 load_dotenv()
@@ -9,10 +9,17 @@ load_dotenv()
 TOKEN = os.environ.get("TOKEN")
 bot = TeleBot(token=TOKEN)
 
-@bot.message_handler(commands=['start'])
+# Re-action to command and send message
+@bot.message_handler(commands=['start1'])
 def start(m):
     bot.send_message(chat_id=m.chat.id, text='Hello my dear, welcome to my bot.')
     bot.set_message_reaction(chat_id=m.chat.id, message_id=m.message_id, reaction=[ReactionTypeEmoji(emoji='😍')])
+
+# Reply to message in another chat
+@bot.message_handler(commands=['start2'])
+def start(m):
+    bot.send_message(chat_id=-1002035951847, text='Hello my dear.', reply_parameters=ReplyParameters(message_id=m.message_id, chat_id=m.chat.id))
+    
 
 
 @bot.message_reaction_handler()
